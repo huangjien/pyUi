7e112b46f41904f64ece3df7a8ee2e2e594fa193
huangjien <huangjien@gmail.com>
2022-04-02 13:53:33 +0100

    add more tabs

--- main.py
+++ main.py
@@ -3,32 +3,54 @@
 # Description: Test Automation Management UI
 
 from tkinter import *
-
+from tkinter import messagebox
 from tkinter import ttk
 
-window = Tk()
+def show_msg(title='warning', msg='Not implemented yet'):
+    messagebox.showinfo(title, msg)
 
+window = Tk()
 window.title("Test Automation Manager")
 
+# tabs
 tab_control = ttk.Notebook(window)
+tabSettings = ttk.Frame(tab_control)
+tabInstances = ttk.Frame(tab_control)
+tabScripts = ttk.Frame(tab_control)
+tabLogs = ttk.Frame(tab_control)
+tab_control.add(tabSettings, text='Settings')
+tab_control.add(tabInstances, text='Instances')
+tab_control.add(tabScripts, text='Scripts')
+tab_control.add(tabLogs, text='Logs')
 
-tab1 = ttk.Frame(tab_control)
+# tabSettings - command buttons line, settings table
+settingsRefreshButton = ttk.Button(tabSettings, text='Refresh', command=show_msg)
+settingsRefreshButton.grid(column=0, row=0)
+settingsSaveButton = ttk.Button(tabSettings, text='Save', command=show_msg)
+settingsSaveButton.grid(column=1, row=0)
 
-tab2 = ttk.Frame(tab_control)
 
-tab_control.add(tab1, text='Settings')
 
-tab_control.add(tab2, text='Instances')
+# tabInstances - command buttons line, instances table
+instancesRefreshButton = ttk.Button(tabInstances, text='Refresh', command=show_msg)
+instancesRefreshButton.grid(column=0, row=0)
+# instancesSaveButton = ttk.Button(tabInstances, text='Save', command=show_msg)
+# instancesSaveButton.grid(column=1, row=0)
 
-lbl1 = Label(tab1, text= 'label1')
+# tabScripts - command buttons line, scripts tree
+scripsRefreshButton = ttk.Button(tabScripts, text='Refresh', command=show_msg)  
+scripsRefreshButton.grid(column=0, row=0)
+scriptsRunButton = ttk.Button(tabScripts, text='Run', command=show_msg)
+scriptsRunButton.grid(column=1, row=0)
 
-lbl1.grid(column=0, row=0)
 
-lbl2 = Label(tab2, text= 'label2')
+# tabLogs - command buttons line, logs table
+logsRefreshButton = ttk.Button(tabLogs, text='Refresh', command=show_msg)
+logsRefreshButton.grid(column=0, row=0)
+logsDeleteButton = ttk.Button(tabLogs, text='Delete', command=show_msg)
+logsDeleteButton.grid(column=1, row=0)
 
-lbl2.grid(column=0, row=0)
 
 tab_control.pack(expand=1, fill='both')
-
-window.geometry('800x600')
+window.geometry('1024x768')
 window.mainloop()
