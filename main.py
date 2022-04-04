6c2c5c93a5c015b554bbf9c2fea30338e1c7edf0
huang_jien <huang_jien@msn.com>
2022-04-04 21:01:17 +0100

    refactor

--- main.py
+++ main.py
@@ -31,44 +31,49 @@ class App(tk.Tk):
         tab_control.add(tabScripts, text='Scripts')
         tab_control.add(tabLogs, text='Logs')
 
-        # tabSettings - command buttons line, settings table
-        settingsRefreshButton = ttk.Button(tabSettings, text='Refresh', command=show_msg)
-        settingsRefreshButton.grid(column=0, row=0)
-        settingsSaveButton = ttk.Button(tabSettings, text='Save', command=show_msg)
-        settingsSaveButton.grid(column=1, row=0)
-        set = ttk.Treeview(tabSettings, height=10, columns=('Setting', 'Value'))
-        set.grid(row=1, sticky='nsew')
-        data = [('Setting 1', 'Value 1'), ('Setting 2', 'Value 2'), ('Setting 3', 'Value 3')]
+        self.initSettingsTab(tabSettings)
 
-        for record in data:
-            set.insert('', 'end', values=record)
+        self.initInstancesTab(tabInstances)
 
+        self.initScriptsTab(tabScripts)
 
+        self.initLogsTab(tabLogs)
 
-        # tabInstances - command buttons line, instances table
-        instancesRefreshButton = ttk.Button(tabInstances, text='Refresh', command=show_msg)
-        instancesRefreshButton.grid(column=0, row=0)
-        # instancesSaveButton = ttk.Button(tabInstances, text='Save', command=show_msg)
-        # instancesSaveButton.grid(column=1, row=0)
+        tab_control.pack(expand=1, fill='both')
 
+    def initScriptsTab(self, tabScripts):
         # tabScripts - command buttons line, scripts tree
         scripsRefreshButton = ttk.Button(tabScripts, text='Refresh', command=show_msg)  
         scripsRefreshButton.grid(column=0, row=0)
         scriptsRunButton = ttk.Button(tabScripts, text='Run', command=show_msg)
         scriptsRunButton.grid(column=1, row=0)
 
+    def initInstancesTab(self, tabInstances):
+        # tabInstances - command buttons line, instances table
+        instancesRefreshButton = ttk.Button(tabInstances, text='Refresh', command=show_msg)
+        instancesRefreshButton.grid(column=0, row=0)
+        # instancesSaveButton = ttk.Button(tabInstances, text='Save', command=show_msg)
+        # instancesSaveButton.grid(column=1, row=0)
 
+    def initLogsTab(self, tabLogs):
         # tabLogs - command buttons line, logs table
         logsRefreshButton = ttk.Button(tabLogs, text='Refresh', command=show_msg)
         logsRefreshButton.grid(column=0, row=0)
         logsDeleteButton = ttk.Button(tabLogs, text='Delete', command=show_msg)
         logsDeleteButton.grid(column=1, row=0)
 
-        tab_control.pack(expand=1, fill='both')
+    def initSettingsTab(self, tabSettings):
+        # tabSettings - command buttons line, settings table
+        settingsRefreshButton = ttk.Button(tabSettings, text='Refresh', command=show_msg)
+        settingsRefreshButton.grid(column=0, row=0)
+        settingsSaveButton = ttk.Button(tabSettings, text='Save', command=show_msg)
+        settingsSaveButton.grid(column=1, row=0)
+        set = ttk.Treeview(tabSettings, height=10, columns=('Setting', 'Value'))
+        set.grid(column=0, row=1, columnspan=4, sticky='ew')
+        data = [('Setting 1', 'Value 1'), ('Setting 2', 'Value 2'), ('Setting 3', 'Value 3')]
 
-
-    
-
+        for record in data:
+            set.insert('', 'end', values=record)
 
 
 if __name__ == '__main__':
